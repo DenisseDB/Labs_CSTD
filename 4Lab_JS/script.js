8cdc163caeb894fc9d8e5d7d723fc655482aa658
Denisse Dominguez <A01702603@itesm.mx>
2022-08-11 12:10:23 -0500

    alcance de variables

--- 4Lab_JS/script.js
+++ 4Lab_JS/script.js
@@ -6,3 +6,16 @@ var pulpo_voice = "Timoty";
 // pruebas optimizadas assert
 // imprimir en la consola el valor de alguna variable
 console.log(pulpo_mundial);
+
+// la variable i muere despues de terminar el ciclo
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// con var la variable no muere nunca
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+// como no murio i, se impime el valor mas alto al que llego despues del ciclo
+// y eso es 10
+console.log(i);
