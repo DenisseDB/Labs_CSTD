a2b06d3b8a174462a103670a44721d8ca5a82ae9
Denisse Dominguez <A01702603@itesm.mx>
2022-08-11 12:02:48 -0500

    imprimir y variables de js

---
+++ 4Lab_JS/script.js
@@ -0,0 +1,8 @@
+// declarar variables con let
+let pulpo_mundial = "Paul";
+// no es recomendable utilizar var
+var pulpo_voice = "Timoty";
+
+// pruebas optimizadas assert
+// imprimir en la consola el valor de alguna variable
+console.log(pulpo_mundial);
