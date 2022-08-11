e35b58db0013039dd498004bf1e93fb97496ab39
Denisse Dominguez <A01702603@itesm.mx>
2022-08-11 12:24:42 -0500

    dif usos de consola

--- 4Lab_JS/script.js
+++ 4Lab_JS/script.js
@@ -5,6 +5,7 @@ var pulpo_voice = "Timoty";
 
 // pruebas optimizadas assert
 // imprimir en la consola el valor de alguna variable
+// uso log para registrar algo en la bitacora
 console.log(pulpo_mundial);
 
 // la variable i muere despues de terminar el ciclo
@@ -19,3 +20,23 @@ for (var i = 0; i < 10; i++) {
 // como no murio i, se impime el valor mas alto al que llego despues del ciclo
 // y eso es 10
 console.log(i);
+
+// info es cuando quiero decir que chequen esto porque hay info importante
+// y aparece el icono de informcion en la consola
+console.info("Esto es infomracion");
+
+// imprimir advertencias con icono y color amarillo
+console.warn("advertencia");
+
+// mandar errores con tache y color rojo
+console.error("error");
+
+// assert sirve para validar algo
+// lo usamos para hacer pruebas del programa y en caso de que
+// la prueba falle, se manda error
+// ASEGURAR QUE LA FUNCION SEA VERDADERA Y FUNCIONE CORRECTAMENTE
+console.assert(1 === 1); // no manda nada porque es true
+console.assert(1 === 2); // manda que la prueba falla, es false
+
+// === compara valor y tipo de dato
+// == compara valor
