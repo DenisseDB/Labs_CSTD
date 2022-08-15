fc7014d44983134a2e2990afd54424e0b2fa41a9
Denisse Dominguez <A01702603@itesm.mx>
2022-08-15 12:10:26 -0500

    boton con cambio

---
+++ 6Lab_Eventos/notas/script.js
@@ -0,0 +1,11 @@
+// DOM es crear los eventos ya cciones
+// es dar click, ooner el pulsro arriba etc
+// permite manupulr a pagina para que tenga el comportamiento que le queremosd dar
+// https://www.w3schools.com/tags/ref_eventattributes.asp
+
+const boton_cambio_animo = document.getElementById("cambio_animo");
+
+// cuando le de click al boton
+boton_cambio_animo.onclick = () => {
+  console.log("funciona el boton");
+};
