410afa5114d1a08963d68a08796baeaaedc5ca3f
Denisse Dominguez <A01702603@itesm.mx>
2022-08-15 12:25:57 -0500

    boton funcionando en cambio de imagenes constante

--- 6Lab_Eventos/notas/script.js
+++ 6Lab_Eventos/notas/script.js
@@ -4,8 +4,36 @@
 // https://www.w3schools.com/tags/ref_eventattributes.asp
 
 const boton_cambio_animo = document.getElementById("cambio_animo");
+// con esta variable validamos el estado del animo y actual acorde en la funcion anonima
+let is_happy = false;
+
+function feliz() {
+  // cambiar el html interno del contenedor_imagen
+  // cambiamos la imagen
+  const cont_contenedor_imagen = document.getElementById("contenedor_imagen");
+  // INNERTEXT -> LO QUE PONGA LO VA A PNER COMO TEXTO
+  // INERHTML PUEDES AGREGAR ATRIBUTOS AL HTML
+  // aqui hacemos la edicion directa con el html
+  cont_contenedor_imagen.innerHTML =
+    '<img src="img/pulpoFeliz.jpeg" alt="pulpo feliz" />';
+  // cambio el texto interno del elemento que estamos obteniendo
+  boton_cambio_animo.innerText = "Triste";
+  is_happy = true;
+}
+
+function triste() {
+  const cont_contenedor_imagen = document.getElementById("contenedor_imagen");
+  cont_contenedor_imagen.innerHTML =
+    '<img src="img/pulpoEnojado.webp" alt="pulpo enojado" />';
+  boton_cambio_animo.innerText = "Feliz";
+  is_happy = false;
+}
 
 // cuando le de click al boton
 boton_cambio_animo.onclick = () => {
-  console.log("funciona el boton");
+  if (is_happy) {
+    triste();
+  } else {
+    feliz();
+  }
 };
