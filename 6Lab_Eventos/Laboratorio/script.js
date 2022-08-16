c8df890a9c544642a419af2d6fc5387f1b656c11
Denisse Dominguez <A01702603@itesm.mx>
2022-08-16 11:46:50 -0500

    carrito de compras y validador completo

--- 6Lab_Eventos/Laboratorio/script.js
+++ 6Lab_Eventos/Laboratorio/script.js
@@ -1,7 +1,7 @@
 const contra1 = document.getElementById("contra1");
 const contra2 = document.getElementById("contra2");
 const verificador = document.getElementById("verificador");
-const total = document - getElementById("total");
+const total = document.getElementById("total");
 
 function validador() {
   // recuperamos el valor de los datos ingresados y hacemos la comparacion
@@ -13,7 +13,25 @@ function validador() {
 }
 
 // boton para verificar, al darle click va a la funcion valdador
+
 verificador.addEventListener("click", validador);
+
+let state = {
+  termo1: {
+    precio: 12,
+    cantidad: 0,
+  },
+  termo2: {
+    precio: 15,
+    cantidad: 0,
+  },
+  termo3: {
+    precio: 10,
+    cantidad: 0,
+  },
+  total: 0,
+  iva: 0,
+};
 
 function anadirCarrito(event) {
   state[event.target.dataset.name].cantidad = event.target.value;
