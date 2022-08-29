67689a24f611d33cb13e4914f7c3a3c7556989ce
Denisse Dominguez <A01702603@itesm.mx>
2022-08-28 19:27:20 -0500

    ejs

--- 12Lab_HTMLDinamico/routes/rutasInformes.routes.js
+++ 12Lab_HTMLDinamico/routes/rutasInformes.routes.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const router = express.Router();
 
+const correosEnviados = [];
+
 router.get("/informes", (request, response, next) => {
   response.sendFile(
     path.join(__dirname, "..", "views", "informes", "envio.html")
@@ -12,16 +14,13 @@ router.get("/informes", (request, response, next) => {
 
 router.post("/informes", (request, response, next) => {
   console.log(request.body);
-  let html =
-    "<!DOCTYPE html>" +
-    '<head><meta charset="UTF-8"></head>' +
-    "<h1>Gracias hemos recibido tu interes del termo " +
-    request.body.termoInteres + // con request.body. agregamos el id del objeto que queremos amnipular obtenido del get
-    " y correo electronico :) </h1>" +
-    "<p> queda atento a tu correo " +
-    request.body.correo +
-    " que en breve nos pondremos en contacto contigo </p>";
-  response.send(html);
+  let correo = "";
+  correo = request.body.correo;
+  correosEnviados.push(correo);
+  response.render(path.join("informes", "correos.ejs"), {
+    correo: correo,
+    correosEnviados: correosEnviados,
+  });
 });
 
 module.exports = router;
